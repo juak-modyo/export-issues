25e8a759c38667a8453e21edd4ee5fe84f647ae2
Joaquín Vergara <joaquin@modyo.com>
2021-04-01 09:46:07 -0300

    Update ghexport.py

--- ghexport.py
+++ ghexport.py
@@ -1,6 +1,4 @@
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 import requests
 import json
 
@@ -21,12 +19,14 @@ def read_milestone():
     
     response = requests.request("GET", url, headers=headers)
     if response.status_code == 200 :
-        print '\nSuccessfully read open milestones on '+url
+        print('\nSuccessfully read open milestones on '+url)
         for miles in json.loads(response.content):
-            ms[miles['title'].encode("utf-8",'replace')] = str(miles['number'])
+            #ms[miles['title'].encode("utf-8",'replace')] = str(miles['number'])
+            ms[miles['title']] = str(miles['number'])
     else:
-        print 'Could not read milestone'
-        print 'Response:', response.content
+        print('Could not read milestone')
+        print('Response:', response.content)
+    print(ms)
     return ms
 
 def read_issues(milestone):
@@ -34,39 +34,38 @@ def read_issues(milestone):
     
     query = "?&milestone="+str(read_milestone()[milestone])+"&state=closed&sort=updated&direction=desc&per_page=100&page=1"
     url = base_url+query
-    f = open(milestone+"-issues.csv", "w")
-    f.write("url,id,milestone,status,title\n")
+    f = open(milestone+"-issues.txt", "w")
+    f.write("#id - title\n")
     Next = True
     while Next:
         response = requests.request("GET", url, headers=headers)
         if response.status_code == 200 :
-            print '\nSuccessfully read issues on '+url
-            if response.links.has_key("next"):
+            print('\nSuccessfully read issues on '+url)
+            if "next" in response.links:
                 url = response.links["next"]["url"]
             else:
                 Next = False
             for issue in json.loads(response.content):
-                if issue.has_key("pull_request"):
+                if "pull_request" in issue:
                     pass
                 else:
                     u = issue['html_url']
                     i = str(issue['number'])
-                    if issue.has_key("milestone"):
+                    if "milestone" in issue:
                         im = issue['milestone']
-                        #print json.dumps(im,indent=4, sort_keys=True)
-                        im = {k: unicode(v).encode("utf-8",'replace') for k,v in im.iteritems()}
+                        im = {k: v for k,v in im.items()}
                         m = issue['milestone']['title']
                     else:
                         m = " "
                     s = issue['state']
                     t = issue['title']
-                    f.write(u+","+i+","+m+","+s+","+t+"\n")
+                    f.write("#"+i+" - "+t+"\n")
         else:
             Next = False
-            print 'Could not read issues'
-            print 'Response:', response.content
+            print('Could not read issues')
+            print('Response:', response.content)
 
 
     f.close()
 
-read_issues("9.1.0")
+read_issues("9.1.14")
